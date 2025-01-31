c696cdac3e1d2d749f4fe4313c40eb8180336ded
LuisFilipeVS <lfilipevs.contato@gmail.com>
2025-01-31 18:28:16 -0300

    Atualizando metodos

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,11 +1,20 @@
 
-<script>
-import WelcomeItem from './WelcomeItem.vue'
+<script setup>
+import { reactive } from 'vue';
 
+const data = reactive([
+  { id: 1, estado: 'inativo', distancia: 0 },
+  { id: 2, estado: 'inativo', distancia: 0 },
+  { id: 3, estado: 'ativo', distancia: 0 },
+  { id: 4, estado: 'inativo', distancia: 0 },
+  { id: 5, estado: 'inativo', distancia: 0 }
+]);
 
-export default {
-    methods: {
-        dataFetch(){
+var leituras = reactive([
+
+]);
+
+const dataFetch = () => {
             fetch('/api') // Substitua pela URL desejada
             .then(response => response.text()) // Obtém o HTML da página como texto
             .then(html => {
@@ -13,29 +22,141 @@ export default {
                 const doc = parser.parseFromString(html, 'text/html'); // Converte o HTML em um documento manipulável
                 const h1Text = doc.querySelector('h1')?.textContent.trim(); // Obtém o texto da tag <h1>
 
-                console.log(h1Text); // Exibe o conteúdo do <h1>
+                //console.log(h1Text); // Exibe o conteúdo do <h1>
+                return h1Text
             })
             .catch(error => console.error('Erro ao buscar a página:', error));
-        }
+            
+};
+        
+const atualizarDados = () => { 
+    var soma = 0
+    data.forEach(element => {
+        element.id = 10;
+        });
+    for (let i = 0; i < 10 ; i++){
+      let leitura = dataFetch();
+      console.log(leitura);
+      leituras.push(leitura);
+      soma += leitura
     }
-}
+    console.log("Media eh " + soma/10 + " Cm");
+    };
+
 </script>
 
+
 <template>
-      <div>
-      <v-toolbar image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-        <v-toolbar-title>Estacionamento Digital</v-toolbar-title>
-
+    <div >
+      <v-toolbar class="menuTopo" image="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+        <v-app-bar-nav-icon style="color: white;"></v-app-bar-nav-icon>
+        <v-toolbar-title style="color: white;">Estacionamento Digital</v-toolbar-title>
         <v-spacer></v-spacer>
-
         <v-btn icon>
-          <v-icon>mdi-export</v-icon>
+          <v-icon style="color: white;">mdi-export</v-icon>
         </v-btn>
       </v-toolbar>
     </div>
-    <v-btn v-on:click="dataFetch">
-        Button
-    </v-btn>
-</template>
+  
+    <div class="containerContent">
+      <div class="body">
+        <div class="titulo">
+          <h3>CONTROLE DE VAGAS</h3>
+        </div>
+  
+        <div class="estacionamento">
+          <div v-for="vaga in data" :key="vaga.id" :class="vaga.estado === 'inativo' ? 'vagaInativa' : 'vagaAtiva'">
+            <div class="vagaTexto">{{ vaga.estado }} - {{ vaga.id }}</div>
+            <div class="vagaIcone">
+              <v-icon v-if="vaga.estado === 'inativo'">mdi-close-circle</v-icon>
+              <v-icon v-else>mdi-circle</v-icon> 
+            </div>
+          </div>
+        </div>
+  
+        <div class="containerBotao">
+          <v-btn class="botao" @click="atualizarDados">Atualizar</v-btn>
+        </div>
+      </div>
+    </div>
+  </template>
+
+<style>
+
+.menuTopo {
+    color: white;
+}
+
+.body{
+    display: grid;
+    color: white;
+    grid-template-columns: repeat(4, 1fr);
+}
+
+.titulo{
+    vertical-align:middle;
+    text-align: center;
+    grid-column-start: 2;
+    grid-column-end: 3;
+}
+
+.estacionamento{
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-column-start: 2;
+    grid-column-end: 3;
+    grid-row-start: 2;
+    grid-row-end: 3;
+}
+
+.vagaInativa{
+    min-height: 200px;
+    min-width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    margin: 2px;
+    flex-direction: column;
+    padding: 10px;
+    background-color: grey;
+}
+
+.vagaAtiva{
+    min-height: 200px;
+    min-width: 100px;
+    display: flex;
+    align-items:center;
+    flex-direction: column;
+    justify-content: center;
+    margin: 2px;
+    background-color: green;
+}
+
+.containerBotao{
+    grid-column-start: 2;
+    grid-column-end: 3;
+    display: flex;
+    padding: 10px;
+    justify-content: center;
+}
+
+.botao{
+    width: 20%;
+}
+
+.containterContent{
+    display: flex;
+    justify-content: center;
+}
+
+.vaga-texto {
+    width: 100%;
+    text-align: center;
+    margin-bottom: auto;
+}
+
+.vaga-icone {
+    margin-top: auto;
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import './assets/main.css'
 import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+import { library, config } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { aliases, fa} from 'vuetify/iconsets/fa'
 
 // Vuetify
 import 'vuetify/styles'
@@ -11,8 +16,8 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 
-//headers.append('Access-Control-Allow-Origin', 'http://10.0.0.96/');
-//headers.append('Access-Control-Allow-Credentials', 'true');
+config.familyDefault = "classic";
+config.styleDefault = "duotone";
 
 const vuetify = createVuetify({
     components,
